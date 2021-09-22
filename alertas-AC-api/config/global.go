40a900e0a61ad60936db52c8a86a7a854d5c082d
Sebastian Traverso <sebastiantraverso@gmail.com>
2021-09-21 22:49:02 -0300

    finish read config file and server starting

--- alertas-AC-api/config/global.go
+++ alertas-AC-api/config/global.go
@@ -1,18 +1,53 @@
 package config
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
 
-const port int = 5000
-const server string = "localhost"
+	"github.com/spf13/viper"
+)
 
-//func init() {
-//}
+// const port int = 5000
+// const server string = "localhost"
 
-func GetPort() int {
-	return port
+func init() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Printf("Error al leer el archivo de configuración %s", err)
+	}
+	log.Println("Archivo de configuraciòn leido correctamente")
 }
 
-func GetServerAddress() string {
-	addr := fmt.Sprintf("%s:%d", server, GetPort())
-	return addr
+func GetPort() (int, error) {
+	value, ok := viper.Get("PORT").(int)
+	if !ok {
+		return 0, errors.New("GetPort: Error al recuperar la variable PORT")
+	}
+	return value, nil
+}
+
+func getServer() (string, error) {
+	value, ok := viper.Get("SERVER").(string)
+	if !ok {
+		return "", errors.New("getServer: Error al recuperar la variable SERVER")
+	}
+	return value, nil
+}
+
+func GetServerAddress() (string, error) {
+	port, err := GetPort()
+	if err != nil {
+		return "", fmt.Errorf("GetServerAddress: Error - %s", err)
+	}
+
+	server, err := getServer()
+	if err != nil {
+		return "", fmt.Errorf("GetServerAddress: Error - %s", err)
+	}
+
+	addr := fmt.Sprintf("%s:%d", server, port)
+	return addr, nil
 }
